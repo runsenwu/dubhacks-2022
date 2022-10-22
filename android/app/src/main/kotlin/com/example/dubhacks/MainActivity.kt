54ba191b96cbbfdce11c6328f0c5d4ac42bb3e6b
wu-chinese <merkurieffr@gmail.com>
2022-10-22 12:23:11 -0700

    Moved the files

---
+++ android/app/src/main/kotlin/com/example/dubhacks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dubhacks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- dubhacks/android/app/src/main/kotlin/com/example/dubhacks/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dubhacks
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
